d5426d8cad0437e1c37ef09c7f2d2d3e23eefdf2
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-17 19:19:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.to_a 
   reverse_string = ""
-  array.each |word| do
+  array.each do |word| 
     reverse_string += word.reverse
   end
   reverse_string
